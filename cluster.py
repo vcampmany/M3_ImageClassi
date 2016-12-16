00f9bd2d48c32db621f900604ac954ac3e29aab9
pau <paucebr@gmail.com>
2016-12-16 12:11:08 +0100

    hola carola

--- cluster.py
+++ cluster.py
@@ -7,6 +7,6 @@ if __name__ == 'main':
 
 
     current_cmd = 'session_cluster.py 100 30 20 linear 1 None sift'
-    cmd = 'qsub -b y -cwd -V -q %s -l mem_token=%s,mem_free=%s %s' % ( queue_name, memory, memory, current_cmd)
+    cmd = 'qsub -cwd -V -q %s -l mem_token=%s,mem_free=%s %s' % ( queue_name, memory, memory, current_cmd)
 
     print os.popen(cmd).read()
