accca3896caa4f8fde31234be56d5bab4af4c098
pau <paucebr@gmail.com>
2016-12-16 12:02:33 +0100

    cluster code

---
+++ cluster.py
@@ -0,0 +1,12 @@
+import os, glob
+
+if __name__ == 'main':
+    #Cluster parameters
+    queue_name = 'faster_master.q'
+    memory = '5M'
+
+
+    current_cmd = 'session_cluster.py 100 30 20 linear 1 None sift'
+    cmd = 'qsub -b y -cwd -V -q %s -l mem_token=%s,mem_free=%s %s' % ( queue_name, memory, memory, current_cmd)
+
+    print os.popen(cmd).read()
