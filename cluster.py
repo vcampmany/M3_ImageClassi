cfc3fd1b3aa6bcf44949937bc042174b6a5c7d02
pau <paucebr@gmail.com>
2016-12-16 12:15:21 +0100

    aaaa

--- cluster.py
+++ cluster.py
@@ -1,12 +1,14 @@
 import os, glob
 
-if __name__ == 'main':
-    #Cluster parameters
-    queue_name = 'fast.master.q'
-    memory = '5M'
+
+#Cluster parameters
+queue_name = 'fast.master.q'
+memory = '5M'
+
+print 'hello'
+
+current_cmd = 'session_cluster.py 100 30 20 linear 1 None sift'
+cmd = 'qsub -cwd -V -q %s -l mem_token=%s,mem_free=%s %s' % ( queue_name, memory, memory, current_cmd)
 
 
-    current_cmd = 'session_cluster.py 100 30 20 linear 1 None sift'
-    cmd = 'qsub -cwd -V -q %s -l mem_token=%s,mem_free=%s %s' % ( queue_name, memory, memory, current_cmd)
-
-    print os.popen(cmd).read()
+print os.popen(cmd).read()
