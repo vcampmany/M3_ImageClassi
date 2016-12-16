362727fd8214b6a093f6cf3dbc6abe483278cb6b
pau <paucebr@gmail.com>
2016-12-16 15:45:28 +0100

    aaa

--- session_cluster.py
+++ session_cluster.py
@@ -76,8 +76,8 @@ def test_SVM(FEATdetector, test_images_filenames, test_labels, clf, stdSlr, redu
 		if reducer:
 			des = reducer.transform(des)
 		predictions = clf.predict(stdSlr.transform(des))
-		values = np.unique(predictions)
-		counts = np.bincount(predictions)
+		values, pos = np.unique(predictions, return_inverse=True)
+		counts = np.bincount(pos)        
 		predictedclass = values[np.argmax(counts)]
 		print 'image '+filename+' was from class '+test_labels[i]+' and was predicted '+predictedclass
 		numtestimages+=1
