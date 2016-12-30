a8101a7158ce63ff92c5dfcbe2d1f70e59d52f51
Guillem Cucurull <gcucurull@gmail.com>
2016-12-30 18:16:46 +0100

    bag of words pyramid

--- week2/cross_val.py
+++ week2/cross_val.py
@@ -10,7 +10,7 @@ from codebooks import compute_codebook
 from utils import get_cross_val_dataset
 from features import features_detector
 
-def main(nfeatures=100, code_size=512, n_components=60, kernel='linear', C=1, reduction=None, features='sift'):
+def main(nfeatures=100, code_size=512, n_components=60, kernel='linear', C=1, reduction=None, features='sift', pyramid=False):
 	start = time.time()
 
 	# read the train and test files
@@ -35,14 +35,18 @@ def main(nfeatures=100, code_size=512, n_components=60, kernel='linear', C=1, re
 			print 'Reading image '+filename
 			ima=cv2.imread(filename)
 			gray=cv2.cvtColor(ima,cv2.COLOR_BGR2GRAY)
-			# kpt = SIFTdetector.detect(gray,None)
-			# kpt,des=SIFTdetector.compute(gray,kpt)
 
-			kpt, des = SIFTdetector.detect_compute(gray)
+			# depending on the parameter pyramid, the variables returned are different
+			# for pyramid=False, kpt and des are lists of keypoints and descriptors (1D)
+			# for pyramid=True, kpt and des are a list of 4 lists of keypoints and descriptors (2D)
+			kpt, des = SIFTdetector.detect_compute(gray, pyramid)
 
 			folds_descriptors[index]['descriptors'].append(des)
 			folds_descriptors[index]['label_per_descriptor'].append(fold[1][i])
-			print str(len(kpt))+' extracted keypoints and descriptors'
+			if pyramid:
+				print str(kpt.shape[0]*kpt.shape[1])+' extracted keypoints and descriptors'
+			else:
+				print str(len(kpt))+' extracted keypoints and descriptors'
 
 	accuracies = []
 	# now perform de cross-val 
@@ -57,22 +61,57 @@ def main(nfeatures=100, code_size=512, n_components=60, kernel='linear', C=1, re
 				Train_descriptors.extend(folds_descriptors[j]['descriptors'])
 				train_labels.extend(folds_descriptors[j]['label_per_descriptor'])
 
-		size_descriptors=Train_descriptors[0].shape[1]
-		D=np.zeros((np.sum([len(p) for p in Train_descriptors]),size_descriptors),dtype=np.uint8)
+		if pyramid:
+			size_descriptors=Train_descriptors[0].shape[2]
+			D=np.zeros((np.sum([len(p.reshape(-1, size_descriptors)) for p in Train_descriptors]),size_descriptors),dtype=np.uint8)
+		else:
+			size_descriptors=Train_descriptors[0].shape[1]
+			D=np.zeros((np.sum([len(p) for p in Train_descriptors]),size_descriptors),dtype=np.uint8)
+
+		#D=np.zeros((np.sum([len(p) for p in Train_descriptors]),size_descriptors),dtype=np.uint8)
 		startingpoint=0
 		for i in range(len(Train_descriptors)):
-			D[startingpoint:startingpoint+len(Train_descriptors[i])]=Train_descriptors[i]
-			startingpoint+=len(Train_descriptors[i])
+			descriptor_i = Train_descriptors[i].reshape(-1, size_descriptors)
+			D[startingpoint:startingpoint+len(descriptor_i)]=descriptor_i
+			startingpoint+=len(descriptor_i)
 
 		# Compute Codebook
 		codebook = compute_codebook(D, code_size, nfeatures, fold_i, features)
 
 		# get visual words from BoW model
 		init=time.time()
-		visual_words=np.zeros((len(Train_descriptors),code_size),dtype=np.float32)
+		if pyramid:
+			visual_words=np.zeros((len(Train_descriptors),code_size*5),dtype=np.float32)
+		else:
+			visual_words=np.zeros((len(Train_descriptors),code_size),dtype=np.float32)
 		for i in xrange(len(Train_descriptors)):
-			words=codebook.predict(Train_descriptors[i])
-			visual_words[i,:]=np.bincount(words,minlength=code_size)
+			if pyramid:
+				# global level
+				words=codebook.predict(Train_descriptors[i].reshape(-1, size_descriptors))
+				visual_words[i,:code_size]=np.bincount(words,minlength=code_size)
+
+				# grid square 1
+				words=codebook.predict(Train_descriptors[i][0])
+				visual_words[i,code_size:2*code_size]=np.bincount(words,minlength=code_size)
+
+				# grid square 2
+				words=codebook.predict(Train_descriptors[i][1])
+				visual_words[i,2*code_size:3*code_size]=np.bincount(words,minlength=code_size)
+
+				# grid square 3
+				words=codebook.predict(Train_descriptors[i][2])
+				visual_words[i,3*code_size:4*code_size]=np.bincount(words,minlength=code_size)
+				
+				# grid square 4
+				words=codebook.predict(Train_descriptors[i][3])
+				visual_words[i,4*code_size:]=np.bincount(words,minlength=code_size)
+			else:
+				words=codebook.predict(Train_descriptors[i].reshape(-1, size_descriptors))
+				visual_words[i,:]=np.bincount(words,minlength=code_size)
+
+			# print(words.shape)
+			# print(visual_words[i,:].shape)
+			# quit()
 
 		end=time.time()
 		print 'Done in '+str(end-init)+' secs.'
@@ -88,16 +127,42 @@ def main(nfeatures=100, code_size=512, n_components=60, kernel='linear', C=1, re
 		# get all the test data and predict their labels
 		test_images_desc = folds_descriptors[fold_i]['descriptors']
 		test_labels = folds_descriptors[fold_i]['label_per_descriptor']
-		visual_words_test=np.zeros((len(test_images_desc),code_size),dtype=np.float32)
+		#visual_words_test=np.zeros((len(test_images_desc),code_size),dtype=np.float32)
+
+		if pyramid:
+			visual_words_test=np.zeros((len(test_images_desc),code_size*5),dtype=np.float32)
+		else:
+			visual_words_test=np.zeros((len(test_images_desc),code_size),dtype=np.float32)
+
 		for i in range(len(test_images_desc)):
 			# filename=test_images_filenames[i]
 			# print 'Reading image '+filename
 			# ima=cv2.imread(filename)
 			# gray=cv2.cvtColor(ima,cv2.COLOR_BGR2GRAY)
 			# kpt,des=SIFTdetector.detectAndCompute(gray,None)
-			words=codebook.predict(test_images_desc[i])
-			visual_words_test[i,:]=np.bincount(words,minlength=code_size)
+			if pyramid:
+				# global level
+				words=codebook.predict(test_images_desc[i].reshape(-1, size_descriptors))
+				visual_words_test[i,:code_size]=np.bincount(words,minlength=code_size)
 
+				# grid square 1
+				words=codebook.predict(test_images_desc[i][0])
+				visual_words_test[i,code_size:2*code_size]=np.bincount(words,minlength=code_size)
+
+				# grid square 2
+				words=codebook.predict(test_images_desc[i][1])
+				visual_words_test[i,2*code_size:3*code_size]=np.bincount(words,minlength=code_size)
+
+				# grid square 3
+				words=codebook.predict(test_images_desc[i][2])
+				visual_words_test[i,3*code_size:4*code_size]=np.bincount(words,minlength=code_size)
+				
+				# grid square 4
+				words=codebook.predict(test_images_desc[i][3])
+				visual_words_test[i,4*code_size:]=np.bincount(words,minlength=code_size)
+			else:
+				words=codebook.predict(test_images_desc[i].reshape(-1, size_descriptors))
+				visual_words_test[i,:]=np.bincount(words,minlength=code_size)
 
 		accuracy = 100*clf.score(stdSlr.transform(visual_words_test), test_labels)
 
@@ -121,8 +186,9 @@ parser.add_argument('-kern', help='SVM kernel to use', type=str, default='linear
 parser.add_argument('-C', help='SVM C parameter', type=float, default=1.0)
 parser.add_argument('-reduce', help='Feature reduction', type=str, default=None)
 parser.add_argument('-feats', help='Features to use', type=str, default='sift')
+parser.add_argument('--pyramid', dest='pyramid', action='store_true')
 args = parser.parse_args()
 
 print(args)
 
-main(args.n_feat, args.code_size, args.n_comp, args.kern, args.C,  args.reduce, args.feats)
+main(args.n_feat, args.code_size, args.n_comp, args.kern, args.C, args.reduce, args.feats, args.pyramid)
--- week2/features.py
+++ week2/features.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 
 class CustomDetector(object):
 	"""docstring for CustomDetector"""
@@ -14,6 +15,8 @@ class CustomDetector(object):
 			self.detector = cv2.SIFT(nfeatures=self.nfeatures)
 		elif self.features == 'dense_sift':
 			self.detector = cv2.FeatureDetector_create("Dense")
+			# dense.setDouble('initFeatureScale', 10)
+			# dense.setInt('initXyStep', 3)
 
 	def set_extractor(self):
 		if self.features == 'sift':
@@ -21,7 +24,7 @@ class CustomDetector(object):
 		elif self.features == 'dense_sift':
 			self.extractor = cv2.SIFT()
 
-	def detect_compute(self, gray):
+	def get_descriptors(self, gray):
 		if self.features == 'sift':
 			kpt = self.detector.detect(gray,None)
 			kpt,des=self.extractor.compute(gray,kpt)
@@ -30,6 +33,32 @@ class CustomDetector(object):
 			kpt,des=self.extractor.compute(gray,kpt)
 
 		return kpt, des
+
+	def detect_compute(self, gray, pyramid=False):
+		if pyramid:
+			kpt = []
+			des = []
+			middle_x = int(gray.shape[0]/2.0)
+			middle_y = int(gray.shape[1]/2.0)
+			
+			# extract keyoints and descriptors by level in a 2x2 grid
+			#########
+			# 1 | 2 #
+			# ----- #
+			# 3 | 4 #
+			#########
+
+			level1_kpt, level1_des = self.get_descriptors(gray[:middle_x, :middle_y])
+			level2_kpt, level2_des = self.get_descriptors(gray[middle_x:, :middle_y])
+			level3_kpt, level3_des = self.get_descriptors(gray[:middle_x, middle_y:])
+			level4_kpt, level4_des = self.get_descriptors(gray[middle_x:, middle_y:])
+
+			kpt = np.asarray([level1_kpt, level2_kpt, level3_kpt, level4_kpt])
+			des = np.asarray([level1_des, level2_des, level3_des, level4_des])
+		else:
+			kpt, des = self.get_descriptors(gray)
+		return kpt, des
+		
 		
 def features_detector(nfeatures=100, features='sift'):
 	detector = CustomDetector(features, nfeatures)
