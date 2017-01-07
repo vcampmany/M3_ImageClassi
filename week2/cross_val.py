13a277d30d30694e716e906e93b12be340de65e0
pau <paucebr@gmail.com>
2017-01-07 08:22:27 +0100

    refactorization

--- week2/cross_val.py
+++ week2/cross_val.py
@@ -11,19 +11,7 @@ from utils import get_cross_val_dataset, normalize_vector
 from features import features_detector
 from svm_kernels import histogram_intersection
 
-def main(nfeatures=100, code_size=512, n_components=60, kernel='linear', C=1, reduction=None, features='sift', pyramid=False):
-	start = time.time()
-
-	# read the train and test files
-	folds_data = get_cross_val_dataset()
-
-	# create the SIFT detector object
-	SIFTdetector = features_detector(nfeatures, features)
-
-	# read all the images per train
-	# extract SIFT keypoints and descriptors
-	# store descriptors in a python list of numpy arrays
-
+def getDescriptors(SIFTdetector, folds_data, pyramid):
 	folds_descriptors = {}
 
 	for index,fold in enumerate(folds_data):
@@ -49,25 +37,68 @@ def main(nfeatures=100, code_size=512, n_components=60, kernel='linear', C=1, re
 			else:
 				print str(len(kpt))+' extracted keypoints and descriptors'
 
+	return folds_descriptors
+
+class siftDetector(object):
+	def getInitData(self, descriptor):
+		size_descriptors=descriptor[0].shape[1]
+		D=np.zeros((np.sum([len(p) for p in descriptor]),size_descriptors),dtype=np.uint8)
+
+		return size_descriptors, D
+
+	def getVisualWords(self, codebook, descriptor, size_descriptors, code_size):
+		visual_words=np.zeros((len(descriptor),code_size),dtype=np.float32)
+
+		for i in xrange(len(descriptor)):
+			words=codebook.predict(descriptor[i].reshape(-1, size_descriptors))
+			visual_words[i,:]=np.bincount(words,minlength=code_size)
+
+		return visual_words
+
+class pyramidDetector(object):
+	def getInitData(self, descriptor):
+		size_descriptors=descriptor[0].shape[2]
+		D=np.zeros((np.sum([len(p.reshape(-1, size_descriptors)) for p in descriptor]),size_descriptors),dtype=np.uint8)
+
+		return size_descriptors, D
+
+	def getVisualWords(self, codebook, descriptor, size_descriptors, code_size):
+		visual_words=np.zeros((len(descriptor),code_size*5),dtype=np.float32)
+
+		for i in xrange(len(descriptor)):
+			words=codebook.predict(descriptor[i].reshape(-1, size_descriptors))
+			visual_words[i,:code_size] = np.bincount(words,minlength=code_size)
+			visual_words[i,:code_size] = normalize_vector(visual_words[i,:code_size]) # normalize
+
+			for j in range(4):
+				words=codebook.predict(descriptor[i][j])
+				visual_words[i,codesize*(j+1):code_size*(j+2)] = np.bincount(words,minlength=code_size)
+				visual_words[i,codesize*(j+1):code_size*(j+2)] = normalize_vector(visual_words[i,codesize*(j+1):code_size*(j+2)]) # normalize
+
+		return visual_words
+	
+def getDetector(pyramid):
+	if pyramid:
+		return pyramidDetetor()
+	else:
+		return siftDetector()
+
+
+def getCrossVal(folds_num, folds_descriptors, start, nfeatures, code_size, kernel, C, features, pyramid):
 	accuracies = []
-	# now perform de cross-val 
-	for fold_i in range(5): # 5 folds
+	for fold_i in range(folds_num): # 5 folds
 		# Transform everything to numpy arrays
 		Train_descriptors = []
 		train_labels = []
 
 		# select training images
-		for j in range(5):
+		for j in range(folds_num):
 			if fold_i != j:
 				Train_descriptors.extend(folds_descriptors[j]['descriptors'])
 				train_labels.extend(folds_descriptors[j]['label_per_descriptor'])
 
-		if pyramid:
-			size_descriptors=Train_descriptors[0].shape[2]
-			D=np.zeros((np.sum([len(p.reshape(-1, size_descriptors)) for p in Train_descriptors]),size_descriptors),dtype=np.uint8)
-		else:
-			size_descriptors=Train_descriptors[0].shape[1]
-			D=np.zeros((np.sum([len(p) for p in Train_descriptors]),size_descriptors),dtype=np.uint8)
+		detector = getDetector(pyramid)
+		size_descriptors, D = detector.getInitData(Train_descriptors)
 
 		#D=np.zeros((np.sum([len(p) for p in Train_descriptors]),size_descriptors),dtype=np.uint8)
 		startingpoint=0
@@ -81,43 +112,8 @@ def main(nfeatures=100, code_size=512, n_components=60, kernel='linear', C=1, re
 
 		# get visual words from BoW model
 		init=time.time()
-		if pyramid:
-			visual_words=np.zeros((len(Train_descriptors),code_size*5),dtype=np.float32)
-		else:
-			visual_words=np.zeros((len(Train_descriptors),code_size),dtype=np.float32)
-		for i in xrange(len(Train_descriptors)):
-			if pyramid:
-				# global level
-				words=codebook.predict(Train_descriptors[i].reshape(-1, size_descriptors))
-				visual_words[i,:code_size]=np.bincount(words,minlength=code_size)
-				visual_words[i,:code_size] = normalize_vector(visual_words[i,:code_size]) # normalize
 
-				# grid square 1
-				words=codebook.predict(Train_descriptors[i][0])
-				visual_words[i,code_size:2*code_size]=np.bincount(words,minlength=code_size)
-				visual_words[i,code_size:2*code_size] = normalize_vector(visual_words[i,code_size:2*code_size]) # normalize
-
-				# grid square 2
-				words=codebook.predict(Train_descriptors[i][1])
-				visual_words[i,2*code_size:3*code_size]=np.bincount(words,minlength=code_size)
-				visual_words[i,2*code_size:3*code_size] = normalize_vector(visual_words[i,2*code_size:3*code_size]) # normalize
-
-				# grid square 3
-				words=codebook.predict(Train_descriptors[i][2])
-				visual_words[i,3*code_size:4*code_size]=np.bincount(words,minlength=code_size)
-				visual_words[i,3*code_size:4*code_size] = normalize_vector(visual_words[i,3*code_size:4*code_size]) # normalize
-				
-				# grid square 4
-				words=codebook.predict(Train_descriptors[i][3])
-				visual_words[i,4*code_size:]=np.bincount(words,minlength=code_size)
-				visual_words[i,4*code_size:] = normalize_vector(visual_words[i,4*code_size:]) # normalize
-			else:
-				words=codebook.predict(Train_descriptors[i].reshape(-1, size_descriptors))
-				visual_words[i,:]=np.bincount(words,minlength=code_size)
-
-			# print(words.shape)
-			# print(visual_words[i,:].shape)
-			# quit()
+		visual_words = detector.getVisualWords(codebook, Train_descriptors, size_descriptors, code_size)
 
 		end=time.time()
 		print 'Done in '+str(end-init)+' secs.'
@@ -126,7 +122,7 @@ def main(nfeatures=100, code_size=512, n_components=60, kernel='linear', C=1, re
 		stdSlr = StandardScaler().fit(visual_words)
 		D_scaled = stdSlr.transform(visual_words)
 		print 'Training the SVM classifier...'
-		if kernel == 'linear'
+		if kernel == 'linear':
 			clf = svm.SVC(kernel=kernel, C=C).fit(D_scaled, train_labels)
 		#elif kernel == 'intersection':
 		#	ker_matrix = histogram_intersection(D_scaled, D_scaled)
@@ -139,45 +135,7 @@ def main(nfeatures=100, code_size=512, n_components=60, kernel='linear', C=1, re
 		test_labels = folds_descriptors[fold_i]['label_per_descriptor']
 		#visual_words_test=np.zeros((len(test_images_desc),code_size),dtype=np.float32)
 
-		if pyramid:
-			visual_words_test=np.zeros((len(test_images_desc),code_size*5),dtype=np.float32)
-		else:
-			visual_words_test=np.zeros((len(test_images_desc),code_size),dtype=np.float32)
-
-		for i in range(len(test_images_desc)):
-			# filename=test_images_filenames[i]
-			# print 'Reading image '+filename
-			# ima=cv2.imread(filename)
-			# gray=cv2.cvtColor(ima,cv2.COLOR_BGR2GRAY)
-			# kpt,des=SIFTdetector.detectAndCompute(gray,None)
-			if pyramid:
-				# global level
-				words=codebook.predict(test_images_desc[i].reshape(-1, size_descriptors))
-				visual_words_test[i,:code_size]=np.bincount(words,minlength=code_size)
-				visual_words_test[i,:code_size] = normalize_vector(visual_words_test[i,:code_size]) # normalize
-
-				# grid square 1
-				words=codebook.predict(test_images_desc[i][0])
-				visual_words_test[i,code_size:2*code_size]=np.bincount(words,minlength=code_size)
-				visual_words_test[i,code_size:2*code_size] = normalize_vector(visual_words_test[i,code_size:2*code_size]) # normalize
-
-				# grid square 2
-				words=codebook.predict(test_images_desc[i][1])
-				visual_words_test[i,2*code_size:3*code_size]=np.bincount(words,minlength=code_size)
-				visual_words_test[i,2*code_size:3*code_size] = normalize_vector(visual_words_test[i,2*code_size:3*code_size]) # normalize
-
-				# grid square 3
-				words=codebook.predict(test_images_desc[i][2])
-				visual_words_test[i,3*code_size:4*code_size]=np.bincount(words,minlength=code_size)
-				visual_words_test[i,3*code_size:4*code_size] = normalize_vector(visual_words_test[i,3*code_size:4*code_size]) # normalize
-				
-				# grid square 4
-				words=codebook.predict(test_images_desc[i][3])
-				visual_words_test[i,4*code_size:]=np.bincount(words,minlength=code_size)
-				visual_words_test[i,4*code_size:] = normalize_vector(visual_words_test[i,4*code_size:]) # normalize
-			else:
-				words=codebook.predict(test_images_desc[i].reshape(-1, size_descriptors))
-				visual_words_test[i,:]=np.bincount(words,minlength=code_size)
+		visual_words_test = detector.getVisualWords(codebook, test_images_desc, size_descriptors, code_size)
 
 		accuracy = 100*clf.score(stdSlr.transform(visual_words_test), test_labels)
 
@@ -191,6 +149,30 @@ def main(nfeatures=100, code_size=512, n_components=60, kernel='linear', C=1, re
 		## 49.56% in 285 secs.
 
 	accuracies = np.asarray(accuracies)
+	return accuracies
+
+
+def main(nfeatures=100, code_size=512, n_components=60, kernel='linear', C=1, reduction=None, features='sift', pyramid=False):
+	start = time.time()
+
+	# read the train and test files
+	folds_data = get_cross_val_dataset()
+
+	# create the SIFT detector object
+	SIFTdetector = features_detector(nfeatures, features)
+
+	# read all the images per train
+	# extract SIFT keypoints and descriptors
+	# store descriptors in a python list of numpy arrays
+
+	folds_descriptors = getDescriptors(SIFTdetector, folds_data, pyramid)
+
+	folds_num = 5
+	accuracies = getCrossVal(folds_num, folds_descriptors, start, nfeatures, code_size,  kernel, C,features, pyramid)
+	
+	
+	# now perform de cross-val
+
 	print('Final accuracy: %f (%f)' % (np.mean(accuracies), np.std(accuracies)))
 
 parser = argparse.ArgumentParser()
