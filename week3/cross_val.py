fixed normalization

week3/cross_val.py
@@ -6,7 +6,7 @@ from sklearn import svm
 from sklearn import preprocessing
 
 import argparse
-from utils import get_cross_val_dataset, normalize_vector
+from utils import get_cross_val_dataset, normalize_vector, l2_normalize_vector
 from data import getFoldsDescriptors, features_detector, PCA_reduce
 from yael import ynumpy
 from codebooks import compute_codebook
@@ -56,16 +56,16 @@ def getCrossVal(folds_num, folds_descriptors, start, nfeatures, code_size, kerne
 				else:
 					des = Train_descriptors[i][j] # for pyramid level j
 				fisher[i,j*k*D.shape[1]*2:(j+1)*k*D.shape[1]*2]= ynumpy.fisher(gmm, np.float32(des), include = ['mu','sigma'])
-				# fisher[i,:]= l2
+				#fisher[i,:] = l2_normalize_vector(fisher[i,:])
 
 		end=time.time()
 		print 'Done in '+str(end-init)+' secs.'
 
 
 		# Train a linear SVM classifier
-
 		stdSlr = StandardScaler().fit(fisher)
 		D_scaled = stdSlr.transform(fisher)
+
 		print 'Training the SVM classifier...'
 		
 		if kernel == 'pyramid_match':
@@ -89,15 +89,17 @@ def getCrossVal(folds_num, folds_descriptors, start, nfeatures, code_size, kerne
 				if reduction == 'pca':
 					des = pca_reducer.transform(des)
 				fisher_test[i,j*k*D.shape[1]*2:(j+1)*k*D.shape[1]*2]=ynumpy.fisher(gmm, np.float32(des), include = ['mu','sigma'])
+				#fisher_test[i,:] = l2_normalize_vector(fisher_test[i,:])
 
-		
+		fisher_test = stdSlr.transform(fisher_test)
+		#fisher_test = l2_normalize_vector(fisher_test)
 		if kernel == 'pyramid_match':
-			predictMatrix = spatialPyramidKernel(stdSlr.transform(fisher_test), D_scaled, code_size, pyramid)
+			predictMatrix = spatialPyramidKernel(fisher_test, D_scaled, code_size, pyramid)
 			#predictions = clf.predict(predictMatrix)
 			#predictions_proba = clf.predict_proba(predictMatrix)
 			accuracy = 100*clf.score(predictMatrix, test_labels)
 		else:
-			accuracy = 100*clf.score(stdSlr.transform(fisher_test), test_labels)
+			accuracy = 100*clf.score(fisher_test, test_labels)
 
 		print 'Fold '+str(fold_i)+' accuracy: ' + str(accuracy)
 